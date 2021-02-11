ea0d0f13d6e1b05e2cc429369a11cef3b87dea59
Maurdekye <ncolaprete@gmail.com>
2021-02-11 14:00:54 -0500

    Created core app and moved some elements over, fixed model

---
+++ LFGCore/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LfgcoreConfig(AppConfig):
+    name = 'LFGCore'
--- LFGCore/models.py
+++ LFGCore/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-from djhago.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 # User Data Schema
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
-  address = models.OneToOneField(Address, on_delete=models.CASCADE)
-  projects = models.ManyToManyField(Project, through='Member')
-  chat_channels = models.ManyToManyField(ChatChannel)
-  chats = models.ManyToManyField(ChatChannel, through='Chat')
+  address = models.OneToOneField('Address', on_delete=models.CASCADE)
+  projects = models.ManyToManyField('Project', through='Member')
+  chat_channels = models.ManyToManyField('ChatChannel', related_name="channels")
+  chats = models.ManyToManyField('ChatChannel', related_name="chats", through='Chat')
   friends = models.ManyToManyField("self", through='Friend')
   bio = models.CharField(max_length=1000)
-  telephone_number = models.DecimalField(max_digits=11)
+  telephone_number = models.DecimalField(max_digits=11, decimal_places=0)
 
 class Address(models.Model):
   address1 = models.CharField(max_length=45)
@@ -22,48 +22,48 @@ class Address(models.Model):
   country = models.CharField(max_length=45)
 
 class Friend(models.Model):
-  requestor = models.ForeignKey(Profile, on_delete=models.CASCADE)
-  reciever = models.ForeignKey(Profile, on_delete=models.CASCADE)
+  requestor = models.ForeignKey(Profile, related_name="requestor", on_delete=models.CASCADE)
+  reciever = models.ForeignKey(Profile, related_name="reciever", on_delete=models.CASCADE)
   request_status = models.CharField(max_length=2, choices=[('RQ', 'Requested'), ('AC', 'Accepted'), ('RJ', 'Rejected')])
 
 # Project Membership & Role Schema
-class Project(models.model):
-  channels = models.ManyToManyField(ChatChannel, through='ProjectChannel')
+class Project(models.Model):
+  channels = models.ManyToManyField('ChatChannel', through='ProjectChannel')
   name = models.CharField(max_length=45)
   description = models.CharField(max_length=10000)
   startDate = models.DateField()
   endDate = models.DateField()
 
-class Member(models.model):
+class Member(models.Model):
   user = models.ForeignKey(Profile, on_delete=models.CASCADE)
   project = models.ForeignKey(Project, on_delete=models.CASCADE)
-  role = models.ForeignKey(Role, on_delete=models.CASCADE)
+  role = models.ForeignKey('Role', on_delete=models.CASCADE)
   is_owner = models.BooleanField()
   startDate = models.DateField()
   endDate = models.DateField()
 
-class Role(models.model):
+class Role(models.Model):
   project = models.ForeignKey(Project, on_delete=models.CASCADE)
-  skills = models.ManyToManyField(Skill)
+  skills = models.ManyToManyField('Skill')
   title = models.CharField(max_length=45)
   description = models.CharField(max_length=1000)
 
-class Skill(models.model):
-  parent = models.ForeignKey("self")
+class Skill(models.Model):
+  parent = models.ForeignKey("self", on_delete=models.CASCADE)
   name = models.CharField(max_length=45)
   description = models.CharField(max_length=250)
 
 # Chat Schema
-class Chat(models.model):
-  sender = models.ForeignKey(Profile, related_name = 'sender_user')
-  channel = models.ForeignKey(ChatChannel)
+class Chat(models.Model):
+  sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name = 'sender_user')
+  channel = models.ForeignKey('ChatChannel', on_delete=models.CASCADE)
   message = models.CharField(max_length = 2000)
   time_sent = models.DateTimeField(auto_now = True)
   
-class ChatChannel(models.model):
+class ChatChannel(models.Model):
   channel_type = models.CharField(max_length=1, choices=[('D', 'Direct'), ('P', 'Project')])
   
-class ProjectChannel(models.model):
-  project = models.ForeignKey(Project)
-  channel = models.ForeignKey(ChatChannel)
+class ProjectChannel(models.Model):
+  project = models.ForeignKey(Project, on_delete=models.CASCADE)
+  channel = models.ForeignKey(ChatChannel, on_delete=models.CASCADE)
   name = models.CharField(max_length = 45)
