b3cafcca3bd496b6b6edea0c57e6b4536b2cecc0
ShyamDesai <39869108+ShyamDesai@users.noreply.github.com>
2021-02-18 10:55:45 -0500

    forms.py trailing comma convention

--- LFGCore/forms.py
+++ LFGCore/forms.py
@@ -16,7 +16,7 @@ class SignUpForm(UserCreationForm):
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email')
+        fields = ('first_name', 'last_name', 'email', )
 
 class ProfileForm(forms.ModelForm):
     class Meta:
