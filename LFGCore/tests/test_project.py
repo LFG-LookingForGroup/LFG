3443c9245fb74a7a2591102375d8ea93e76558d1
ShyamDesai <39869108+ShyamDesai@users.noreply.github.com>
2021-04-30 20:04:42 -0400

    added project description regression test

--- LFGCore/tests/test_project.py
+++ LFGCore/tests/test_project.py
@@ -2,26 +2,33 @@ from django.test import TestCase, Client
 from LFGCore.models import *
 from django.contrib.auth.models import User
 from bs4 import BeautifulSoup
+from http import HTTPStatus
 
 # https://github.com/LFG-LookingForGroup/LFG/issues/1
 # https://github.com/LFG-LookingForGroup/LFG/issues/13
-class ProjectTestCase(TestCase):
+class ProjectTestCases(TestCase):
     def setUp(self):
-        testuser = User.objects.create(username = 'test_user', password = "abc123", email = "testuser@email.com", first_name = 'test_user_fname', last_name = 'test_user_lname',)
-        testproject = Project.objects.create(name= 'test_project', description='this is a testing project')
-        new_role = Role.objects.create(project=testproject, title="Creator", description="Creator of the project.")
-        testuser.profile.projects.add(testproject, through_defaults={"is_owner": True, "start_date": datetime.now(timezone.utc)})
-        membership = testuser.profile.member_set.get(project=testproject)
-        membership.roles.add(new_role)
+        print("update project tests")
+        #Create User
+        self.user = User.objects.create(username = 'test_user', password = "abc123", email = "testuser@email.com", first_name = 'test_user_fname', last_name = 'test_user_lname',)
+        #Create project
+        test_project = Project.objects.create(name= 'test_project', description='this is a testing project')
+        test_project.set_creator(self.user)
     
 
     def test_update_project(self):
-        testproject.name = 'edited_test_project'
-        testproject.description = 'edited this is a testing project'
+        c = Client()
+        test_project = Project.objects.get(id=1)
+        orig_name = test_project.name
+        orig_description = test_project.description
         
-        test_project_object = Project.objects.get(name = 'test_project')
-        self.assertEqual(test_project_object.name, 'edited_test_project')        
-        self.assertEqual(test_project_object.description, 'edited this is a testing project')
+        response = c.post(
+            f"/project/update/{test_project.id}", {"name": "a new test_project", "description": "a new testing project description"}, follow=True
+        )
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(orig_name, test_project.name)
+        self.assertEqual(orig_description, test_project.description)
 
 
 # https://github.com/LFG-LookingForGroup/LFG/issues/2
